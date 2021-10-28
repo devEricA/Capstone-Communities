559086d52f64b185ee07e054bcdc5b94ea4688f2
devEricA <eric.anderson413@gmail.com>
2021-10-28 12:18:15 -0500

    Add placeholders for database functions

--- Communities/app/controllers/app.go
+++ Communities/app/controllers/app.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"image/color"
+	// _ "github.com/go-sql-driver/mysql"
 	"github.com/flopp/go-staticmaps"
 	"github.com/fogleman/gg"
 	"github.com/golang/geo/s2"
@@ -11,6 +12,18 @@ import (
 type App struct {
 	*revel.Controller
 }
+
+type User struct {
+	User_ID      int64
+	Display_Name string
+	Bio          string
+}
+
+var CurrentSess User                  //User info
+var LoggedIn bool                     //Login success
+var NoAcc = "Wrong Email or Password" //Fail String
+var LoginSuccess = "Welcome " 
+// var db *sql.DB
 
 const (
 	addr     = "Lubbock, TX"
@@ -21,12 +34,48 @@ func (c App) Index() revel.Result {
 	//Consider this as our "Starter Function Area"
 	//Instantiate the database here!
 
+	/*
+	InitDB()
+	*/
 	return c.Redirect(App.Login)
 }
 
 func (c App) Login() revel.Result {
+	// c.Flash.Error("Can I set these off?")
 	return c.Render()
 }
+
+/*
+func (c App) LogValidate(LoginUserName string, LoginPassword string) revel.Result{
+	if(DBLogin(LoginUserName, LoginPassword, CurrentSess)){
+		LoggedIn = true; 
+		return c.Redirect(App.Home)
+	}
+	else{
+		c.Flash.Error("Invalid Username or Password")
+		return c.Redirect(App.Login)
+	}
+}
+
+func (c App) CreateAccount(NewUserName string, NewPassword string, Email string,) revel.Result{
+	if(DBCreateAccount(NewUserName, NewPassword, NewEmail, CurrentSess){
+		c.Flash.Success("Account Created! You may login now. ")
+		return c.Redirect(App.Login)
+	}
+	else{
+		c.Flash.Error("Error occured when creating the account.")
+		return c.Redirect(App.AccountCreation)
+	}
+}
+
+func (c App) CreatePost() revel.Result{
+
+}
+
+func (c App) CreateCommunity() revel.Result{
+
+}
+*/
 
 func (c App) Home(LoginUserName string, LoginPassword string) revel.Result{
 	c.Validation.Required(LoginUserName).Message("Username is required.")
@@ -41,6 +90,7 @@ func (c App) Home(LoginUserName string, LoginPassword string) revel.Result{
     }
 
 	createMap(lat, lng)
+	//TODO: Render user communities, latest posts, and communities on the map
 	return c.Render(LoginUserName, LoginPassword)
 }
  
@@ -61,6 +111,7 @@ func (c App) CreateCommunity() revel.Result{
 }
 
 func (c App) Community() revel.Result{
+	//TODO: Render Community Name, Posts, Description, and Events
 	return c.Render()
 }
 
@@ -110,3 +161,70 @@ func createMap(lat float64, lng float64) {
 		panic(err)
 	}
 }
+
+/*
+//Attempts to login user. QueryRow throws error if no user + pass combo found
+func DBLogin(Username int, Password string, CurrentSess User) bool {
+	var err error
+
+	UserSearch := db.QueryRow("SELECT User_ID, Display_Name, Bio FROM User WHERE User_ID = ? AND Password = ? ", &Username, &Password)
+	switch err = UserSearch.Scan(&CurrentSess.User_ID, &CurrentSess.Display_Name, &CurrentSess.Bio); err {
+	case sql.ErrNoRows:
+		return false
+	case nil:
+		return true
+	default:
+		panic(err)
+	}
+}
+
+//Searches for an email associated with an account and then returns whether one was found or not
+func DBAccRecovery(Email string) string {
+	var err error
+	var AccountExist int
+	var NoAcc = "Account Not Found"
+	var YesAcc = "Recovery email sent!"
+
+	UserSearch, err := db.Query("SELECT COUNT(User_Email) FROM User WHERE User_Email = ? ", &Email)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	UserSearch.Scan(&AccountExist)
+	UserSearch.Close()
+
+	if AccountExist == 0 {
+		return NoAcc
+	} else {
+		return YesAcc
+	}
+}
+
+//Searches for an account already reigstered with an email and then will attempt to create account or return an account already exists
+func DBCreateAccount(Username string, Password string, Email string, CurrentSess User) bool {
+	var err error
+	var AccountExist int
+	var AccExis = "An account already exists with that email."
+	var AccCreated = "Account created succesfully."
+
+	UserSearch, err := db.Query("SELECT COUNT(User_Email) FROM User WHERE User_Email = ? ", &Email)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	UserSearch.Scan(&AccountExist)
+	UserSearch.Close()
+
+	if AccountExist != 0 {
+		return false
+	}
+
+	_, err = db.Exec("INSERT INTO User (User_ID, Password, Email) VALUES (?, ?, ?)", &Username, &Password, &Email)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+
+}
+*/
